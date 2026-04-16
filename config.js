993ec8420fe621135ccae7ed84e56edb13a9c052
Frederico G C Matos <fredibs.cell@gmail.com>
2026-04-15 21:26:34 -0300

    f-tech

--- config.js
+++ config.js
@@ -2,13 +2,13 @@
 const CONFIG = {
     // Chave do Web3Forms (obtenha em https://web3forms.com)
     WEB3FORMS_KEY: "7d2fe79d-bcc4-4597-abab-8b1506b75e44",
-
+    
     // Número do WhatsApp (apenas números, sem espaços ou caracteres especiais)
     WHATSAPP_NUMBER: "38997389585",
-
+    
     // Mensagem padrão do WhatsApp
     WHATSAPP_MESSAGE: "Olá! Gostaria de mais informações sobre os serviços da F-Tech Solutions.",
-
+    
     // Links das redes sociais
     SOCIAL_INSTAGRAM: "https://www.instagram.com/ftechlab",
     SOCIAL_LINKEDIN: "https://www.linkedin.com/in/frederico-gonçalves-carneiro-matos-330a4896",
